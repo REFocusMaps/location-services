62a3c6279af8eff2e86345ad31fa98f0fcb44099
Austin Mueller <armueller91@gmail.com>
2020-09-29 18:57:23 -0600

    Working on deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,9 +7,10 @@ GIT_HASH_SHORT=$(git rev-parse --short HEAD)
 if [[ "$GIT_STATUS" =~ .*(nothing to commit, working tree clean).* ]] ; then
     echo "Clean and synced: $GIT_HASH_LONG"
     echo "Updating .env with git hash"
-    printf "GIT_HASH=${GIT_HASH_SHORT}\n" > .env.prod
+    sed -i '' "s/GIT_HASH=.*/GIT_HASH=$GIT_HASH_SHORT/" .env.prod
+    git commit -am "Updated GIT_HASH to $GIT_HASH_SHORT for deployment"
+    AWS_PROFILE=refm-prod sls deploy --env prod
 else
     echo "Cannot deploy unless all changes are commited."
     exit 1
 fi
-# AWS_PROFILE=refm-prod sls deploy --env prod
