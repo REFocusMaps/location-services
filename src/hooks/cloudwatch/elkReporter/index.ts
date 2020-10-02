13582158fe3089ed69bf14afa48bbfa80b899128
Austin Mueller <armueller91@gmail.com>
2020-10-01 19:54:37 -0600

    Working on elk cloudwatch log hook

--- src/hooks/cloudwatch/elkReporter/index.ts
+++ src/hooks/cloudwatch/elkReporter/index.ts
@@ -108,8 +108,16 @@ async function reportEvent(serviceName: string, timestamp: Date, eventType: stri
                 },
             };
             console.log(elkPayload);
-            const result = await elasticClient.index(elkPayload);
+            const result = await elasticClient.index(elkPayload,
+                {
+                    ignore: [404],
+                    maxRetries: 3
+                }, (err, result) => {
+                    console.log(result);
+                    if (err) console.log(err);
+                });
             const x = await elasticClient.ping();
+            console.log('AFTER! NEXT IS THE RESULT!');
             console.log(result);
         } else {
             console.log('No elasticsearch client, skipping.');
